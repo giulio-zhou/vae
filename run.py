28e2ce4d253ecb8cdbc427d7106f3d370dcf4fbd
Giulio Zhou <giuliozhou8@gmail.com>
2018-06-02 10:50:07 -0700

    Refactor to make latent_dim settable

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import os
 import skimage.io as skio
 import tensorflow as tf
 
-def vae(data_dim):
+def vae(data_dim, latent_dim):
     inputs = tf.placeholder(tf.float32, [None, data_dim])
     batch_size = tf.shape(inputs)[0]
     # Declare decoder components.
@@ -14,15 +14,15 @@ def vae(data_dim):
     # Encode.
     enc_fc1 = tf.layers.dense(inputs, units=500, activation=tf.nn.relu)
     enc_fc2 = tf.layers.dense(enc_fc1, units=200, activation=tf.nn.relu)
-    mean_fc3 = tf.layers.dense(enc_fc2, units=10)
-    stddev_fc3 = tf.layers.dense(enc_fc2, units=10) # log(sigma^2)
+    mean_fc3 = tf.layers.dense(enc_fc2, units=latent_dim)
+    stddev_fc3 = tf.layers.dense(enc_fc2, units=latent_dim) # log(sigma^2)
     # Apply reparameterization towards sampling.
     mvn_sampler = tf.contrib.distributions.MultivariateNormalDiag(
-        loc=tf.zeros(10))
+        loc=tf.zeros(latent_dim))
     samples = mvn_sampler.sample(batch_size)
     samples = tf.sqrt(tf.exp(stddev_fc3)) * samples + mean_fc3
     # Share parameters between decoder used for training and inference.
-    decoder_inputs = tf.placeholder(tf.float32, [None, 10])
+    decoder_inputs = tf.placeholder(tf.float32, [None, latent_dim])
     decoder_output = decoder_inputs
     decoder_train_output = samples
     for decoder_layer in [dec_fc1, dec_fc2, dec_fc3]:
@@ -48,18 +48,21 @@ def run():
     batch_size = 100
     num_iters = 10000
     # Declare model.
-    inputs, decoder_train_output, decoder_inputs, decoder_output, loss, trainer = vae(784)
+    latent_dim = 10
+    inputs, decoder_train_output, decoder_inputs, \
+            decoder_output, loss, trainer = vae(784, latent_dim)
     sess = tf.Session()
     sess.run(tf.global_variables_initializer())
     # Visualization points.
     num_viz, num_cols = 21, 20
-    xs = np.random.multivariate_normal(np.zeros(10), np.eye(10), num_viz)
+    xs = np.random.multivariate_normal(
+        np.zeros(latent_dim), np.eye(latent_dim), num_viz)
     rows = []
     for i in range(num_viz - 1):
         step = (xs[i+1] - xs[i]) / float(num_cols)
         rows.append([xs[i] + step*j for j in range(num_cols)])
-    rows = np.array(rows).reshape(-1, 10)
-    output_dir = 'out'
+    rows = np.array(rows).reshape(-1, latent_dim)
+    output_dir = 'out2'
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
     # Train.
@@ -67,7 +70,7 @@ def run():
         idx = np.random.choice(np.arange(len(data)), batch_size, replace=False)
         X_batch = data[idx]
         _, batch_loss = sess.run([trainer, loss], {inputs: X_batch})
-        if t % 100 == 0:
+        if t % 200 == 0:
             print(t, batch_loss)
             decoded_outputs = sess.run(decoder_output,
                                        {decoder_inputs: rows})
