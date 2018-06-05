a9a581616e25d7a9dc482536345df8f4ee0baa0c
Giulio Zhou <giuliozhou8@gmail.com>
2018-06-04 17:44:24 -0700

    Add ORL faces dataset

---
+++ get_orl_faces.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget http://www.cl.cam.ac.uk/Research/DTG/attarchive/pub/data/att_faces.tar.Z
+tar -xvf att_faces.tar.Z
+rm att_faces.tar.Z
--- util.py
+++ util.py
@@ -1,6 +1,20 @@
 from sklearn.datasets import fetch_mldata
+from PIL import Image
+import numpy as np
 
 def get_mnist_data():
-  mnist_path = 'mnist'
-  mnist = fetch_mldata('MNIST original', data_home=mnist_path)
-  return mnist['data'].reshape(-1, 28, 28), mnist['target']
+    mnist_path = 'mnist'
+    mnist = fetch_mldata('MNIST original', data_home=mnist_path)
+    return mnist['data'].reshape(-1, 28, 28), mnist['target']
+
+def get_orl_face_data():
+    root_dir = 'orl_faces'
+    imgs, labels = [], []
+    for subject_id in range(40):
+        for img_idx in range(10):
+            img_path = '%s/s%d/%d.pgm' % (root_dir, subject_id + 1, img_idx + 1)
+            print(img_path)
+            img = np.array(Image.open(img_path))
+            imgs.append(img)
+            labels.append(subject_id)
+    return np.array(imgs), np.array(labels)
