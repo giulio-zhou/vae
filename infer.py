9cc027199d002aff43c85ec417e4fdc79cb9ed86
Giulio Zhou <giuliozhou8@gmail.com>
2018-06-06 15:51:49 -0400

    Interpolation between actual samples

--- infer.py
+++ infer.py
@@ -39,5 +39,39 @@ def compare_mean_vs_samples():
                           (j+2)*img_width:(j+3)*img_width] = decoded_X_sample
     skio.imsave(output_path, output_buffer)
 
+def interpolate_between_classes():
+    data, labels = get_mnist_data() 
+    data = data.reshape(-1, 784) / 255.
+    # Settings.
+    model_dir = 'out/model@10000'
+    img_height, img_width = 28, 28
+    input_dim = 784
+    latent_dim = 10
+    num_classes = 10
+    num_steps = 10
+    output_path = 'interpolate_classes.png'
+    # Model.
+    vae_model = VAE(vae, input_dim, latent_dim)
+    vae_model.load_model(model_dir)
+    # Data to examine.
+    class_order = np.arange(num_classes)
+    np.random.shuffle(class_order)
+    class_idx = [np.random.choice(np.where(labels == c)[0]) \
+                 for c in range(num_classes)]
+    xs = vae_model.encode(data[class_idx])
+    # Create visualization.
+    output_buffer = np.zeros((img_height * (num_classes - 1),
+                              img_width * num_steps))
+    for c in range(num_classes-1):
+        step = (xs[c+1] - xs[c]) / float(num_steps)
+        X = np.expand_dims(xs[c], axis=0)
+        for i in range(num_steps):
+            decoded_X = vae_model.decode(X + i*step)[0]
+            decoded_X = decoded_X.reshape(img_height, img_width)
+            output_buffer[c*img_height:(c+1)*img_height,
+                          i*img_width:(i+1)*img_width] = decoded_X
+    skio.imsave(output_path, output_buffer)
+
 if __name__ == '__main__':
     compare_mean_vs_samples()
+    interpolate_between_classes()
