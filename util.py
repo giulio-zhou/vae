07514193df3392042b4e2c70f9f4ba32868d28d6
Giulio Zhou <giuliozhou8@gmail.com>
2018-06-15 12:35:48 -0400

    Add new data sources

--- util.py
+++ util.py
@@ -1,5 +1,6 @@
 from sklearn.datasets import fetch_mldata, fetch_olivetti_faces
 from PIL import Image
+import h5py
 import numpy as np
 
 def get_mnist_data():
@@ -11,3 +12,15 @@ def get_olivetti_data():
     olivetti_path = 'olivetti'
     face_data = fetch_olivetti_faces(olivetti_path)
     return face_data.images, face_data.target
+
+def get_train_data():
+    path = '/home/giulio/train_cam_full_64x64.h5'
+    hdf5_file = h5py.File(path, 'r')
+    data, labels = hdf5_file['data'], None
+    return data, labels
+
+def get_jh_data():
+    path = '/home/giulio/new_jh_1_and_3_64x64_5fps.h5'
+    hdf5_file = h5py.File(path, 'r')
+    data, labels = hdf5_file['data'], hdf5_file['labels']
+    return data, labels
